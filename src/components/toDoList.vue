7326c40abb768a9c4a1bc3439bd7675c61fc6049
Mila-byte <tamila.r.1998@gmail.com>
2021-09-09 17:51:32 +0300

    first commit

---
+++ src/components/toDoList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "toDoList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='main-layout'>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
+
+<style></style>
